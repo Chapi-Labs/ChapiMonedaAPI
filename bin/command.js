17b63f4efe6584d5e284558e3749245e31010676
Pablo <pablo5_diaz@hotmail.com>
2017-01-29 00:30:56 -0600

    test command

---
+++ bin/command.js
@@ -0,0 +1,23 @@
+#! /app/bin/node
+export const callBI = () => {
+  const form = {
+    action: 'getMoneda'
+  };
+
+  const formData = querystring.stringify(form);
+
+  request({
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
+    },
+    uri: 'https://www.corporacionbi.com/service/mod_moneda.php',
+    body: formData,
+    method: 'POST'
+  }, (err, response, body) => {
+    console.log(body);
+  });
+
+}
+callBI();
+process.exit()
