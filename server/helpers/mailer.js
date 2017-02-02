272d5fcbd8488892f2e981859d55eedd2a47b699
Pablo <pablo5_diaz@hotmail.com>
2017-02-02 09:12:14 -0600

    add mailer

---
+++ server/helpers/mailer.js
@@ -0,0 +1,24 @@
+import nodemailer from 'nodemailer';
+import mg from 'nodemailer-mailgun-transport';
+
+// This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
+const auth = {
+  auth: {
+    api_key: 'key-be6d50f0bce7a323b08af0adc3713036',
+    domain: 'mailgun.newtonlabs.com.gt'
+  }
+};
+
+const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+
+const sendMail = (emailer, content) => {
+  nodemailerMailgun.sendMail({
+    from: 'notifications@newtonlabs.com.gt',
+    to: emailer, // An array if you have multiple recipients.
+    subject: 'Cambió el tipo de cambio !',
+    text: content
+  });
+};
+
+export default sendMail;
+
